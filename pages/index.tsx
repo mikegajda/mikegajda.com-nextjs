182d1f33b0449d3f0d3af4e659543b408b3d4856
Mike Gajda <github@mikegajda.com>
2022-02-12 03:30:51 +0000

    nav changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from 'date-fns';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 import Layout from '../components/Layout';
 import { getAllPosts } from '../lib/api';
@@ -14,23 +15,32 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
   return (
     <Layout>
       {posts.map((post) => (
-        <article key={post.slug} className="mt-12">
-          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+        <article key={post.slug} className="mt-12 border-2 rounded-md">
+          <div className="bg-gray-200 p-3 text-sm text-gray-500 dark:text-gray-400">
             {format(parseISO(post.date), 'MMMM dd, yyyy')}
-          </p>
-          <h1 className="mb-2 text-xl">
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a className="text-gray-900 dark:text-white dark:hover:text-emerald-400 ">
-                {post.title}
-              </a>
-            </Link>
-          </h1>
-          <p className="mb-3">{post.description}</p>
-          <p>
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a>Read More</a>
-            </Link>
-          </p>
+          </div>
+          <Image
+            alt={`Bridge`}
+            src={`/images/bridge.jpg`}
+            width={1440}
+            height={960}
+            priority
+          />
+          <div className="p-3">
+            <h1 className="mb-2 text-xl">
+              <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+                <a className="text-gray-900 dark:text-white dark:hover:text-emerald-400 ">
+                  {post.title}
+                </a>
+              </Link>
+            </h1>
+            <p className="mb-3">{post.description}</p>
+            <p>
+              <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+                <a>Read More</a>
+              </Link>
+            </p>
+          </div>
         </article>
       ))}
     </Layout>
