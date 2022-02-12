fa54b31a28144311ce2ecd0675e8234520ba95be
Mike Gajda <github@mikegajda.com>
2022-02-12 15:15:22 +0000

    basic pagination

--- lib/api.ts
+++ lib/api.ts
@@ -35,11 +35,39 @@ export function getPostBySlug(slug: string, fields: string[] = []): PostItems {
   return items;
 }
 
-export function getAllPosts(fields: string[] = []): PostItems[] {
+const fields = ['date', 'description', 'slug', 'title', 'image', 'type'];
+
+const MAX_POSTS = 1;
+
+export function getAllPosts(): PostItems[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
+}
+
+function paginate(arr, size) {
+  return arr.reduce((acc, val, i) => {
+    const idx = Math.floor(i / size);
+    const page = acc[idx] || (acc[idx] = []);
+    page.push(val);
+
+    return acc;
+  }, []);
+}
+
+export function getSortedFilteredPaginatedPosts(
+  filter: (a: PostItems) => boolean,
+  maxPosts: number = MAX_POSTS
+): PostItems[][] {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    .filter(filter)
+    // sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  const pages = paginate(posts, maxPosts);
+  return pages;
 }
