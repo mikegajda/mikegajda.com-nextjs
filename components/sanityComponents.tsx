54059c40d1212ebaaadcf93742e71de687e5613d
Mike Gajda <github@mikegajda.com>
2022-04-27 02:20:59 +0000

    remove header prose overrides

--- components/sanityComponents.tsx
+++ components/sanityComponents.tsx
@@ -5,12 +5,12 @@ import Gallery from './Gallery';
 export const globalComponents: PortableTextComponents = {
   block: {
     // Ex. 1: customizing common block types
-    h1: ({ children }) => <h1 className="prose">{children}</h1>,
-    h2: ({ children }) => <h2 className="prose">{children}</h2>,
-    h3: ({ children }) => <h3 className="prose">{children}</h3>,
-    h4: ({ children }) => <h4 className="prose">{children}</h4>,
-    h5: ({ children }) => <h5 className="prose">{children}</h5>,
-    h6: ({ children }) => <h6 className="prose">{children}</h6>,
+    h1: ({ children }) => <h1 className="">{children}</h1>,
+    h2: ({ children }) => <h2 className="">{children}</h2>,
+    h3: ({ children }) => <h3 className="">{children}</h3>,
+    h4: ({ children }) => <h4 className="">{children}</h4>,
+    h5: ({ children }) => <h5 className="">{children}</h5>,
+    h6: ({ children }) => <h6 className="">{children}</h6>,
     p: ({ children }) => <p className="prose">{children}</p>,
     blockquote: ({ children }) => (
       <blockquote className="px-3 py-2 border-emerald-400 dark:border-emerald-600 border-l-2 bg-gray-300 my-3 prose-blockquote dark:bg-gray-800 ">
