aaa565d9d2f7f1025850b58a0b84fa22c9b05011
Mike Gajda <github@mikegajda.com>
2023-03-06 04:01:23 +0000

    first pass at sanity upgrade

---
+++ sanity/sanity.config.js
@@ -0,0 +1,15 @@
+// sanity.config.js
+import { defineConfig } from "sanity";
+import { deskTool } from 'sanity/desk';
+import schemas from './schemas/schema';
+import { visionTool } from '@sanity/vision'
+
+export default defineConfig({
+  title: "mikegajda-next-js",
+  projectId: "0zzwwluw",
+  dataset: "production",
+  plugins: [deskTool(), visionTool()],
+  schema: {
+    types: [schemas],
+  },
+});
